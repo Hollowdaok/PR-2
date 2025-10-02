526bc8a75b7bde55b181cbaf89689955e5e623de
Hollowdaok <didilivsk@gmail.com>
2025-10-02 15:43:15 +0300

    Change class actual_record.py

--- actual_record.py
+++ actual_record.py
@@ -1,10 +1,12 @@
 from academic_record import AcademicRecord
 
 class ActualRecord(AcademicRecord):
-    def calculate_mean(self):
-        all_scores = self.retrieve_scores()
-        if len(all_scores) == 0:
-            return 0.0
-        total = sum(all_scores)
-        count = len(all_scores)
-        return total / count
+    def calculate_mean_per_course(self):
+        means = {}
+        for course in self.retrieve_courses():
+            scores = self.retrieve_scores()[course]
+            if len(scores) == 0:
+                means[course] = 0.0
+            else:
+                means[course] = sum(scores) / len(scores)
+        return means

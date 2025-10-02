cca157a5068e625e969a62acc8bac5aaa2495d3f
Hollowdaok <didilivsk@gmail.com>
2025-10-02 15:42:34 +0300

    Change class academic_record.py

--- academic_record.py
+++ academic_record.py
@@ -1,16 +1,16 @@
 from abc import ABC, abstractmethod
 
 class AcademicRecord(ABC):
-    def __init__(self, course_list: list[str], score_list: list[float]):
+    def __init__(self, course_list: list[str], score_dict: dict[str, list[float]]):
         self.__course_list = course_list
-        self.__score_list = score_list
+        self.__score_dict = score_dict
 
     def retrieve_courses(self):
         return self.__course_list
 
     def retrieve_scores(self):
-        return self.__score_list
+        return self.__score_dict
 
     @abstractmethod
-    def calculate_mean(self):
+    def calculate_mean_per_course(self):
         pass

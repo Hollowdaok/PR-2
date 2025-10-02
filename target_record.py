c0c7270b5258ccc5679d6c034e806dbbfffa0a8c
Hollowdaok <didilivsk@gmail.com>
2025-10-02 15:46:32 +0300

    Change class target_record.py

--- target_record.py
+++ target_record.py
@@ -1,9 +1,9 @@
 from academic_record import AcademicRecord
 
 class TargetRecord(AcademicRecord):
-    def __init__(self, course_list: list[str], score_list: list[float], target_mean: float):
-        super().__init__(course_list, score_list)
-        self.__target_mean = target_mean
+    def __init__(self, course_list: list[str], score_dict: dict[str, list[float]], target_means: dict[str, float]):
+        super().__init__(course_list, score_dict)
+        self.__target_means = target_means
 
-    def calculate_mean(self):
-        return self.__target_mean
+    def calculate_mean_per_course(self):
+        return self.__target_means
